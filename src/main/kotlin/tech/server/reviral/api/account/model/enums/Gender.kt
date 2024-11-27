c5710f6d56b8d6a4155565c7a386d08303a02a1c
Developerbabe <118235272+BE-AlexKim@users.noreply.github.com>
2024-11-27 19:14:03 +0900

    열거형 Gender, UserRole 생성

---
+++ src/main/kotlin/tech/server/reviral/api/account/model/enums/Gender.kt
@@ -0,0 +1,16 @@
+package tech.server.reviral.api.account.model.enums
+
+/**
+ *packageName    : tech.server.reviral.api.account.model
+ * fileName       : Gender
+ * author         : joy58
+ * date           : 2024-11-26
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-11-26        joy58       최초 생성
+ */
+enum class Gender {
+    MAN,WOMEN
+}
---
+++ src/main/kotlin/tech/server/reviral/api/account/model/enums/UserRole.kt
@@ -0,0 +1,16 @@
+package tech.server.reviral.api.account.model.enums
+
+/**
+ *packageName    : tech.server.reviral.api.account.model.enums
+ * fileName       : UserRole
+ * author         : joy58
+ * date           : 2024-11-27
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-11-27        joy58       최초 생성
+ */
+enum class UserRole {
+    ROLE_REVIEWER, ROLE_SELLER, ROLE_ADMIN
+}
